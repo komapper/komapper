a10541a58cf9edf3542d83a3c729e12ff14d86d9
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-03-24 18:34:06 +0900

    Release 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,10 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 plugins {
     kotlin("jvm") version "1.4.30" apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
 }
+
+val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
 
 allprojects {
     repositories {
@@ -17,6 +20,8 @@ allprojects {
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
 
     ktlint {
         version.set("0.41.0")
@@ -39,12 +44,71 @@ subprojects {
         kotlinOptions.jvmTarget = "11"
     }
 
+    configure<JavaPluginExtension> {
+        withJavadocJar()
+        withSourcesJar()
+    }
+
     dependencies {
         "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.7.1")
         "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.7.1")
         "testRuntimeOnly"("org.junit.vintage:junit-vintage-engine:5.7.1")
     }
+    
 }
 
+configure(subprojects.filter {it.name != "example"}) {
+
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+                pom {
+                    val projectUrl: String by project
+                    name.set(project.name)
+                    description.set("Kotlin SQL Mapper")
+                    url.set(projectUrl)
+                    licenses {
+                        license {
+                            name.set("The Apache Software License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("nakamura-to")
+                            name.set("Toshihiro Nakamura")
+                            email.set("toshihiro.nakamura@gmail.com")
+                        }
+                    }
+                    scm {
+                        val githubUrl: String by project
+                        connection.set("scm:git:$githubUrl")
+                        developerConnection.set("scm:git:$githubUrl")
+                        url.set(projectUrl)
+                    }
+                }
+            }
+        }
+    }
+
+    configure<SigningExtension> {
+//        val signingKey: String? by project
+//        val signingPassword: String? by project
+//        useInMemoryPgpKeys(signingKey, signingPassword)
+        val publishing = convention.findByType(PublishingExtension::class)!!
+        sign(publishing.publications)
+        isRequired = isReleaseVersion
+    }
+}
 rootProject.apply {
+
+    nexusPublishing {
+        repositories {
+            sonatype {
+                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+                snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            }
+        }
+    }
 }
