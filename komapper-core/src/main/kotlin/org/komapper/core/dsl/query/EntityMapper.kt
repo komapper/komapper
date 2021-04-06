4ac889c7a7d35652296a3c08f49dc670cca70283
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-04-07 07:51:10 +0900

    Polish EntityMapper

--- komapper-core/src/main/kotlin/org/komapper/core/dsl/query/EntityMapper.kt
+++ komapper-core/src/main/kotlin/org/komapper/core/dsl/query/EntityMapper.kt
@@ -6,12 +6,12 @@ import org.komapper.core.dsl.metamodel.PropertyMetamodel
 import java.sql.ResultSet
 
 internal class EntityMapper(val dialect: Dialect, val resultSet: ResultSet) {
-    private var index = 0
+    private val propertyMapper = PropertyMapper(dialect, resultSet)
 
     fun <E> execute(e: EntityMetamodel<E>): E? {
         val valueMap = mutableMapOf<PropertyMetamodel<*, *>, Any?>()
         for (p in e.properties()) {
-            val value = dialect.getValue(resultSet, ++index, p.klass)
+            val value = propertyMapper.execute(p)
             valueMap[p] = value
         }
         if (valueMap.values.all { it == null }) {
