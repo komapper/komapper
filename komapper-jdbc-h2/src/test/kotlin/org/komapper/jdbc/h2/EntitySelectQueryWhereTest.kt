e969a6bb96ccd3aaa0c46f80d6a0df8a95422208
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-04-08 20:57:28 +0900

    Add test for like and notLike predicates

--- komapper-jdbc-h2/src/test/kotlin/org/komapper/jdbc/h2/EntitySelectQueryWhereTest.kt
+++ komapper-jdbc-h2/src/test/kotlin/org/komapper/jdbc/h2/EntitySelectQueryWhereTest.kt
@@ -60,6 +60,28 @@ class EntitySelectQueryWhereTest(private val db: Database) {
     }
 
     @Test
+    fun like() {
+        val a = Address.metamodel()
+        val list = db.execute {
+            EntityQuery.from(a).where {
+                a.street like "STREET 1_"
+            }.orderBy(a.addressId)
+        }
+        assertEquals((10..15).toList(), list.map { it.addressId })
+    }
+
+    @Test
+    fun notLike() {
+        val a = Address.metamodel()
+        val list = db.execute {
+            EntityQuery.from(a).where {
+                a.street notLike "STREET 1_"
+            }.orderBy(a.addressId)
+        }
+        assertEquals((1..9).toList(), list.map { it.addressId })
+    }
+
+    @Test
     fun inList() {
         val a = Address.metamodel()
         val list = db.execute {
@@ -304,161 +326,4 @@ class EntitySelectQueryWhereTest(private val db: Database) {
         val list = db.execute { EntityQuery.from(a).where(w1 + w2) }
         assertEquals(1, list.size)
     }
-
-/*
-@Test
-fun distinct() {
-    val list = db.select<NoId> {
-        distinct()
-    }
-    assertEquals(
-        listOf(
-            NoId(1, 1)
-        ), list
-    )
-}
-
-@Test
-fun like() {
-    val idList = db.select<Address> {
-        where {
-            like(Address::street, "STREET 1_")
-        }
-        orderBy {
-            asc(Address::addressId)
-        }
-    }.map { it.addressId }
-    assertEquals((10..15).toList(), idList)
-}
-
-@Test
-fun notLike() {
-    val idList = db.select<Address> {
-        where {
-            notLike(Address::street, "STREET 1_")
-        }
-        orderBy {
-            asc(Address::addressId)
-        }
-    }.map { it.addressId }
-    assertEquals((1..9).toList(), idList)
-}
-
-@Test
-fun noArg() {
-    val list = db.select<Address>()
-    assertEquals(15, list.size)
-}
-
-
-
-
-@Test
-fun in2() {
-
-    val list = db.select<Address> {
-        where {
-            in2(Address::addressId, Address::street, listOf(9 to "STREET 9", 10 to "STREET 10"))
-        }
-        orderBy {
-            desc(Address::addressId)
-        }
-    }
-    assertEquals(
-        listOf(
-            Address(10, "STREET 10", 1),
-            Address(9, "STREET 9", 1)
-        ), list
-    )
-}
-
-@Test
-fun notIn2() {
-
-    val idList = db.select<Address> {
-        where {
-            notIn2(Address::addressId, Address::street, listOf(1 to "STREET 1", 2 to "STREET 2"))
-        }
-        orderBy {
-            asc(Address::addressId)
-        }
-    }.map { it.addressId }
-    assertEquals((3..15).toList(), idList)
-}
-
-@Test
-fun in2_empty() {
-
-    val list = db.select<Address> {
-        where {
-            in2(Address::addressId, Address::street, emptyList())
-        }
-        orderBy {
-            desc(Address::addressId)
-        }
-    }
-    assertTrue(list.isEmpty())
-}
-
-@Test
-fun in3() {
-
-    val list = db.select<Address> {
-        where {
-            in3(
-                Address::addressId,
-                Address::street,
-                Address::version,
-                listOf(
-                    Triple(9, "STREET 9", 1),
-                    Triple(10, "STREET 10", 1)
-                )
-            )
-        }
-        orderBy {
-            desc(Address::addressId)
-        }
-    }
-    assertEquals(
-        listOf(
-            Address(10, "STREET 10", 1),
-            Address(9, "STREET 9", 1)
-        ), list
-    )
-}
-
-@Test
-fun notIn3() {
-
-    val idList = db.select<Address> {
-        where {
-            notIn3(
-                Address::addressId, Address::street, Address::version,
-                listOf(
-                    Triple(1, "STREET 1", 1),
-                    Triple(2, "STREET 2", 1)
-                )
-            )
-        }
-        orderBy {
-            asc(Address::addressId)
-        }
-    }.map { it.addressId }
-    assertEquals((3..15).toList(), idList)
-}
-
-@Test
-fun in3_empty() {
-
-    val list = db.select<Address> {
-        where {
-            in3(Address::addressId, Address::street, Address::version, emptyList())
-        }
-        orderBy {
-            desc(Address::addressId)
-        }
-    }
-    assertTrue(list.isEmpty())
-}
- */
 }
