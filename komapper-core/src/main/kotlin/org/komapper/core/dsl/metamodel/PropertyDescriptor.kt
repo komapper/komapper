c60045ea29768ee78527208599a193d6edb3c055
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-09 20:49:05 +0900

    Refactor metamodel package

--- komapper-core/src/main/kotlin/org/komapper/core/dsl/metamodel/PropertyDescriptor.kt
+++ komapper-core/src/main/kotlin/org/komapper/core/dsl/metamodel/PropertyDescriptor.kt
@@ -3,15 +3,15 @@ package org.komapper.core.dsl.metamodel
 import kotlin.reflect.KClass
 
 class PropertyDescriptor<ENTITY, EXTERIOR : Any, INTERIOR : Any>(
-    val klass: KClass<EXTERIOR>,
+    val exteriorClass: KClass<EXTERIOR>,
     val interiorClass: KClass<INTERIOR>,
     val name: String,
     val columnName: String,
     val alwaysQuote: Boolean,
     val getter: (ENTITY) -> EXTERIOR?,
     val setter: (ENTITY, EXTERIOR) -> ENTITY,
-    val compose: (INTERIOR) -> EXTERIOR,
-    val decompose: (EXTERIOR) -> INTERIOR,
+    val wrap: (INTERIOR) -> EXTERIOR,
+    val unwrap: (EXTERIOR) -> INTERIOR,
     val nullable: Boolean,
     val idAssignment: Assignment<ENTITY>?
 )
