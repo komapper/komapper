900b0f873e31bde15e367e35b41fe5125fc7723e
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-09 14:55:12 +0900

    Use lowercase table name in PostgreSQL

--- integration-test/src/test/kotlin/integration/MetadataQueryTest.kt
+++ integration-test/src/test/kotlin/integration/MetadataQueryTest.kt
@@ -1,5 +1,6 @@
 package integration
 
+import integration.setting.Dbms
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -10,10 +11,21 @@ import org.komapper.core.dsl.runQuery
 @ExtendWith(Env::class)
 class MetadataQueryTest(val db: Database) {
 
+    @Run(unless = [Dbms.POSTGRESQL])
     @Test
-    fun test() {
+    fun test_uppercase() {
         val table = db.runQuery {
             MetadataDsl.tables(tableNamePattern = "ADDRESS")
+        }.first()
+        println(table)
+        assertEquals(3, table.columns.size)
+    }
+
+    @Run(onlyIf = [Dbms.POSTGRESQL])
+    @Test
+    fun test_lowercase() {
+        val table = db.runQuery {
+            MetadataDsl.tables(tableNamePattern = "address")
         }.first()
         println(table)
         assertEquals(3, table.columns.size)
