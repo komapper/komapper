e8841571835bb7a38419f124199855a9f5898948
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-13 08:43:08 +0900

    Implement the Dsl interface

--- komapper-core/src/main/kotlin/org/komapper/core/dsl/MetadataDsl.kt
+++ komapper-core/src/main/kotlin/org/komapper/core/dsl/MetadataDsl.kt
@@ -3,7 +3,7 @@ package org.komapper.core.dsl
 import org.komapper.core.dsl.query.MetadataQuery
 import org.komapper.core.dsl.query.MetadataQueryImpl
 
-object MetadataDsl {
+object MetadataDsl : Dsl {
 
     fun tables(
         schemaPattern: String? = null,
