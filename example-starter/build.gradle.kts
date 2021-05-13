05009a8ba4813ffa7eb0c36e8e73dc30c1dccbf5
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-13 09:02:46 +0900

    Update dependency com.google.devtools.ksp to v1.5.0-1.0.0-alpha10

--- example-starter/build.gradle.kts
+++ example-starter/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     application
     idea
-    id("com.google.devtools.ksp") version "1.5.0-1.0.0-alpha09"
+    id("com.google.devtools.ksp")
 }
 
 sourceSets {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,11 @@
 pluginManagement {
+    val kspVersion: String by settings
     repositories {
         gradlePluginPortal()
         google()
+    }
+    plugins {
+        id("com.google.devtools.ksp") version kspVersion
     }
 }
 
