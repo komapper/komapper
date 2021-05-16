6a4efea7f2c9a2e75f376dc5c5e015b89ac00c50
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-16 10:55:24 +0900

    Register the komapperGenerator task as aggregate task

--- gradle-plugin/src/main/kotlin/org/komapper/gradle/KomapperPlugin.kt
+++ gradle-plugin/src/main/kotlin/org/komapper/gradle/KomapperPlugin.kt
@@ -4,20 +4,33 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.komapper.gradle.codegen.GenerateTask
 
+@Suppress("unused")
 class KomapperPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         val extension =
             project.extensions.create(KomapperExtension.NAME, KomapperExtension::class.java, project)
+        registerGenerateTasks(project, extension)
+    }
 
+    private fun registerGenerateTasks(project: Project, extension: KomapperExtension) {
+        val description = "Generate entities and definitions"
+        val prefix = KomapperExtension.NAME
+        val suffix = "Generator"
+        val aggregateTask = project.tasks.register(prefix + suffix) {
+            it.description = "$description."
+            it.group = KomapperExtension.NAME
+            it.outputs.upToDateWhen { false }
+        }
         extension.generators.all { generator ->
-            project.tasks
-                .register("komapper${generator.name.capitalize()}Generator", GenerateTask::class.java, generator)
-                .configure {
-                    it.description = "Generate entities and definitions"
-                    it.group = KomapperExtension.NAME
-                    it.outputs.upToDateWhen { false }
-                }
+            val task = project.tasks
+                .register("$prefix${generator.name.capitalize()}$suffix", GenerateTask::class.java, generator)
+            task.configure {
+                it.description = "$description for ${generator.name}."
+                it.group = KomapperExtension.NAME
+                it.outputs.upToDateWhen { false }
+            }
+            aggregateTask.configure { it.dependsOn(task) }
         }
     }
 }
