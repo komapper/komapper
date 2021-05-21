4ec6c8c69a66dc33e91ab0f4659078180ffb8b45
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-22 06:15:30 +0900

    Add jackson-module-kotlin to suppress warning

--- example-spring-boot/build.gradle.kts
+++ example-spring-boot/build.gradle.kts
@@ -21,6 +21,7 @@ idea.module {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation(project(":komapper-ext-spring-boot-starter"))
     ksp(project(":komapper-processor"))
 }
