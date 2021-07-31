80cb60064256d0836e3697bd524b496699ac1797
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-07-31 17:49:05 +0900

    Change dependency configuration from "api" to "runtimeOnly"

--- komapper-starter-r2dbc/build.gradle.kts
+++ komapper-starter-r2dbc/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api(project(":komapper-annotation"))
     api(project(":komapper-r2dbc"))
     api(project(":komapper-tx-r2dbc"))
-    api(project(":komapper-slf4j"))
+    runtimeOnly(project(":komapper-slf4j"))
     runtimeOnly(project(":komapper-template"))
     runtimeOnly("ch.qos.logback:logback-classic:1.2.5")
 }
