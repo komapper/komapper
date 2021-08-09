040c052cb666b6cf3b9c96df866c570231b4128b
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-08-09 12:09:39 +0900

    Publish komapper-slf4j dependency

--- komapper-starter-jdbc/build.gradle.kts
+++ komapper-starter-jdbc/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api(project(":komapper-annotation"))
     api(project(":komapper-jdbc"))
     api(project(":komapper-tx-jdbc"))
-    runtimeOnly(project(":komapper-slf4j"))
+    api(project(":komapper-slf4j"))
     runtimeOnly(project(":komapper-template"))
     runtimeOnly("ch.qos.logback:logback-classic:1.2.5")
 }
