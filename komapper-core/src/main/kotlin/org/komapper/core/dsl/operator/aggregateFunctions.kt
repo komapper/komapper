cebb7d5bea39baf58d4b50b8bc60b64cb44a230d
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-09-22 22:37:32 +0900

    Change parameter names

--- komapper-core/src/main/kotlin/org/komapper/core/dsl/operator/aggregateFunctions.kt
+++ komapper-core/src/main/kotlin/org/komapper/core/dsl/operator/aggregateFunctions.kt
@@ -4,26 +4,26 @@ import org.komapper.core.dsl.expression.AggregateFunction
 import org.komapper.core.dsl.expression.ColumnExpression
 import org.komapper.core.dsl.expression.ScalarExpression
 
-fun avg(c: ColumnExpression<*, *>): ScalarExpression<Double, Double> {
-    return AggregateFunction.Avg(c)
+fun avg(expression: ColumnExpression<*, *>): ScalarExpression<Double, Double> {
+    return AggregateFunction.Avg(expression)
 }
 
 fun count(): ScalarExpression<Long, Long> {
     return AggregateFunction.CountAsterisk
 }
 
-fun count(property: ColumnExpression<*, *>): ScalarExpression<Long, Long> {
-    return AggregateFunction.Count(property)
+fun count(expression: ColumnExpression<*, *>): ScalarExpression<Long, Long> {
+    return AggregateFunction.Count(expression)
 }
 
-fun <T : Any, S : Any> max(property: ColumnExpression<T, S>): ScalarExpression<T, S> {
-    return AggregateFunction.Max(property)
+fun <T : Any, S : Any> max(expression: ColumnExpression<T, S>): ScalarExpression<T, S> {
+    return AggregateFunction.Max(expression)
 }
 
-fun <T : Any, S : Any> min(property: ColumnExpression<T, S>): ScalarExpression<T, S> {
-    return AggregateFunction.Min(property)
+fun <T : Any, S : Any> min(expression: ColumnExpression<T, S>): ScalarExpression<T, S> {
+    return AggregateFunction.Min(expression)
 }
 
-fun <T : Any, S : Any> sum(property: ColumnExpression<T, S>): ScalarExpression<T, S> {
-    return AggregateFunction.Sum(property)
+fun <T : Any, S : Any> sum(expression: ColumnExpression<T, S>): ScalarExpression<T, S> {
+    return AggregateFunction.Sum(expression)
 }
