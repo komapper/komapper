719aec2ef220f48fa77b8d003e5fd536e8b2d9d4
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-10-04 22:46:18 +0900

    Polish SortItem.Column to avoid cyclic package dependency (#230)

--- komapper-core/src/main/kotlin/org/komapper/core/dsl/operator/sortItems.kt
+++ komapper-core/src/main/kotlin/org/komapper/core/dsl/operator/sortItems.kt
@@ -1,15 +1,17 @@
 package org.komapper.core.dsl.operator
 
 import org.komapper.core.dsl.element.SortItem
+import org.komapper.core.dsl.expression.AscendingExpression
 import org.komapper.core.dsl.expression.ColumnExpression
+import org.komapper.core.dsl.expression.DescendingExpression
 import org.komapper.core.dsl.expression.SortExpression
 
 fun <T : Any, S : Any> ColumnExpression<T, S>.asc(): SortExpression {
-    return SortItem.Column.Asc(this)
+    return AscendingExpression(this)
 }
 
 fun <T : Any, S : Any> ColumnExpression<T, S>.desc(): SortExpression {
-    return SortItem.Column.Desc(this)
+    return DescendingExpression(this)
 }
 
 fun asc(alias: CharSequence): CharSequence {
