1c0f7fd237fe456c3cfde4f78b4d437b0a381e5d
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-11-11 00:12:56 +0900

    Polish build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    `maven-publish`
+    signing
     kotlin("jvm")
     id("com.diffplug.spotless") version "6.0.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
@@ -23,7 +25,7 @@ val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
 allprojects {
     apply(plugin = "com.diffplug.spotless")
 
-    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    spotless {
         kotlinGradle {
             ktlint("0.41.0")
         }
@@ -62,13 +64,7 @@ configure(libraryProjects + gradlePluginProject + exampleProjects + integrationT
     }
 
     tasks {
-        withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
-        }
-
-        withType<Test> {
+        withType<Test>().configureEach {
             useJUnitPlatform()
         }
     }
@@ -92,7 +88,7 @@ configure(libraryProjects + platformProject) {
         "java"
     }
 
-    configure<PublishingExtension> {
+    publishing {
         publications {
             create<MavenPublication>("maven") {
                 from(components[component])
@@ -125,7 +121,7 @@ configure(libraryProjects + platformProject) {
         }
     }
 
-    configure<SigningExtension> {
+    signing {
         val signingKey: String? by project
         val signingPassword: String? by project
         useInMemoryPgpKeys(signingKey, signingPassword)
