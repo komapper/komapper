c22ac2af2c839607fe66aea82fc028ac5731e7ef
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-11-14 11:29:26 +0900

    Polish

---
+++ komapper-processor/src/main/kotlin/org/komapper/ksp/ClassNames.kt
@@ -0,0 +1,19 @@
+package org.komapper.ksp
+
+internal object ClassNames {
+    const val ConcurrentHashMap = "java.util.concurrent.ConcurrentHashMap"
+    const val EntityMetamodel = "org.komapper.core.dsl.metamodel.EntityMetamodel"
+    const val EntityMetamodelStub = "org.komapper.core.dsl.metamodel.EntityMetamodelStub"
+    const val EntityMetamodelImplementor = "org.komapper.core.dsl.metamodel.EntityMetamodelImplementor"
+    const val IdAssignment = "org.komapper.core.dsl.metamodel.IdAssignment"
+    const val AutoIncrement = "org.komapper.core.dsl.metamodel.IdAssignment.AutoIncrement"
+    const val Sequence = "org.komapper.core.dsl.metamodel.IdAssignment.Sequence"
+    const val IdContext = "org.komapper.core.dsl.metamodel.IdContext"
+    const val UUID = "java.util.UUID"
+    const val PropertyDescriptor = "org.komapper.core.dsl.metamodel.PropertyDescriptor"
+    const val PropertyMetamodel = "org.komapper.core.dsl.metamodel.PropertyMetamodel"
+    const val PropertyMetamodelImpl = "org.komapper.core.dsl.metamodel.PropertyMetamodelImpl"
+    const val PropertyMetamodelStub = "org.komapper.core.dsl.metamodel.PropertyMetamodelStub"
+    const val Clock = "java.time.Clock"
+    const val EntityDescriptor = "__EntityDescriptor"
+}
---
+++ komapper-processor/src/main/kotlin/org/komapper/ksp/EntityMetamodelStubGenerator.kt
@@ -0,0 +1,54 @@
+package org.komapper.ksp
+
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import org.komapper.ksp.ClassNames.EntityMetamodelImplementor
+import org.komapper.ksp.ClassNames.EntityMetamodelStub
+import org.komapper.ksp.ClassNames.PropertyMetamodel
+import org.komapper.ksp.ClassNames.PropertyMetamodelStub
+import java.io.PrintWriter
+import java.time.ZonedDateTime
+
+internal class EntityMetamodelStubGenerator(
+    private val defDeclaration: KSClassDeclaration,
+    private val declaration: KSClassDeclaration,
+    private val packageName: String,
+    private val entityTypeName: String,
+    private val simpleName: String,
+    private val w: PrintWriter
+) : Runnable {
+    private val constructorParamList = listOf(
+        "table: String = \"\"",
+        "catalog: String = \"\"",
+        "schema: String = \"\"",
+        "alwaysQuote: Boolean = false"
+    ).joinToString(", ")
+
+    override fun run() {
+        if (packageName.isNotEmpty()) {
+            w.println("package $packageName")
+            w.println()
+        }
+        w.println("// generated at ${ZonedDateTime.now()}")
+        w.println("@$EntityMetamodelImplementor")
+        w.println("class $simpleName : $EntityMetamodelStub<$entityTypeName, $simpleName>() {")
+        val parameters = declaration.primaryConstructor?.parameters
+        if (parameters != null) {
+            for (p in parameters) {
+                val typeName = p.type.resolve().declaration.qualifiedName?.asString()
+                w.println("    val $p: $PropertyMetamodel<$entityTypeName, $typeName, $typeName> = $PropertyMetamodelStub<$entityTypeName, $typeName>()")
+            }
+        }
+        w.println("    companion object {")
+        w.println("        val meta = $simpleName()")
+        w.println("        fun newMeta($constructorParamList) = $simpleName()")
+        w.println("    }")
+        w.println("}")
+        val companionObject = defDeclaration.getCompanionObject()
+        if (companionObject != null) {
+            val companionObjectName = (companionObject.qualifiedName ?: companionObject.simpleName).asString()
+            w.println("")
+            w.println("val $companionObjectName.meta get() = $simpleName.meta")
+            w.println("fun $companionObjectName.newMeta($constructorParamList) = $simpleName.newMeta(table, catalog, schema, alwaysQuote)")
+        }
+    }
+}
