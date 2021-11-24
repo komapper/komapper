d3088747b988a11e2dda15e05e5962e7ba1f8f49
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-11-24 22:02:53 +0900

    Don't throw any throwable from the dryRun function

--- komapper-core/src/main/kotlin/org/komapper/core/utility.kt
+++ komapper-core/src/main/kotlin/org/komapper/core/utility.kt
@@ -1,7 +1,7 @@
 package org.komapper.core
 
-fun Statement.toDryRunResult(dialect: Dialect): DryRunResult {
+fun Statement.toDryRunResult(dialect: Dialect, description: String = ""): DryRunResult {
     val sql = this.toSql(dialect::replacePlaceHolder)
     val sqlWithArgs = this.toSqlWithArgs(dialect::formatValue)
-    return DryRunResult(sql, sqlWithArgs, this.args)
+    return DryRunResult(sql = sql, sqlWithArgs = sqlWithArgs, args = this.args, description = description)
 }
