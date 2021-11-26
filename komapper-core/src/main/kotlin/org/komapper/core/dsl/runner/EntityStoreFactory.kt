b062a29d0148a517c6a4875fea7cdce5d8d413ec
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-11-26 23:38:20 +0900

    Use Map instead of MutableMap

--- komapper-core/src/main/kotlin/org/komapper/core/dsl/runner/EntityStoreFactory.kt
+++ komapper-core/src/main/kotlin/org/komapper/core/dsl/runner/EntityStoreFactory.kt
@@ -11,17 +11,20 @@ class EntityStoreFactory<ENTITY : Any, ID : Any, META : EntityMetamodel<ENTITY, 
     private val context: SelectContext<ENTITY, ID, META>,
 ) {
 
-    fun create(rows: List<MutableMap<EntityMetamodel<*, *, *>, Any>>): EntityStore {
+    fun create(rows: List<Map<EntityMetamodel<*, *, *>, Any>>): EntityStore {
         val cache: MutableMap<EntityKey, Any> = mutableMapOf()
+        val newRows = ArrayList<Map<EntityMetamodel<*, *, *>, Any>>(rows.size)
         for (row in rows) {
+            val newRow = LinkedHashMap<EntityMetamodel<*, *, *>, Any>(row.size)
             for ((metamodel, entity) in row) {
                 @Suppress("UNCHECKED_CAST")
                 metamodel as EntityMetamodel<Any, Any, *>
                 val id = metamodel.id(entity)
                 val key = EntityKey(metamodel, id)
                 val prev = cache.putIfAbsent(key, entity)
-                row[metamodel] = prev ?: entity
+                newRow[metamodel] = prev ?: entity
             }
+            newRows.add(newRow)
         }
         return EntityStoreImpl(context, rows)
     }
