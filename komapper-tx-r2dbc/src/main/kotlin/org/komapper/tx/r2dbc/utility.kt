4692d57a8420c6689295573dd6006128bebda9a0
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-12-27 19:03:31 +0900

    Add the TransactionScopeStub class.
    This class is needed to execute non-transactional logic in the test environment.

--- komapper-tx-r2dbc/src/main/kotlin/org/komapper/tx/r2dbc/utility.kt
+++ komapper-tx-r2dbc/src/main/kotlin/org/komapper/tx/r2dbc/utility.kt
@@ -10,13 +10,21 @@ suspend fun <R> R2dbc.withTransaction(
     isolationLevel: IsolationLevel? = null,
     block: suspend TransactionScope.() -> R
 ): R {
-    val database = this as R2dbcDatabase
-    val session = database.config.session
-    return if (session is TransactionSession) {
-        session.userTransaction.transaction(transactionAttribute, isolationLevel, block)
+    return if (this is R2dbcDatabase) {
+        val session = this.config.session
+        return if (session is TransactionSession) {
+            session.userTransaction.transaction(transactionAttribute, isolationLevel, block)
+        } else {
+            withoutTransaction(block)
+        }
     } else {
-        invalidSession(session)
+        withoutTransaction(block)
     }
+}
+
+private suspend fun <R> withoutTransaction(block: suspend TransactionScope.() -> R): R {
+    val transactionScope = TransactionScopeStub()
+    return block(transactionScope)
 }
 
 val R2dbcSession.transactionManager: TransactionManager
