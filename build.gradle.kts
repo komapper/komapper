8579154f100a55fce503779c0416db31018ab37b
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-01-09 12:14:46 +0900

    Format automatically with the build task

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
 val ktlintVersion: String by project
 
 allprojects {
+    apply(plugin = "base")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
 
     ktlint {
@@ -51,6 +52,21 @@ allprojects {
 
     repositories {
         mavenCentral()
+    }
+
+    tasks {
+        val pairs = listOf(
+            "ktlintKotlinScriptCheck" to "ktlintKotlinScriptFormat",
+            "ktlintMainSourceSetCheck" to "ktlintMainSourceSetFormat",
+            "ktlintTestSourceSetCheck" to "ktlintTestSourceSetFormat",
+        )
+        for ((checkTask, formatTask) in pairs) {
+            findByName(checkTask)?.mustRunAfter(formatTask)
+        }
+
+        build {
+            dependsOn("ktlintFormat")
+        }
     }
 }
 
