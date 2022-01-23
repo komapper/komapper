b11f635720a23e4aeaa65c84077c7d0bd6c9c080
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-01-23 16:54:06 +0900

    Use the JVM Test Suite Plugin

--- integration-test-jdbc/build.gradle.kts
+++ integration-test-jdbc/build.gradle.kts
@@ -1,39 +1,89 @@
-val driver: Any by project
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    `jvm-test-suite`
+}
 
 dependencies {
-    implementation(project(":integration-test-core"))
-    implementation(project(":komapper-tx-jdbc"))
-    implementation("org.postgresql:postgresql:42.3.1")
-    testImplementation(platform("org.testcontainers:testcontainers-bom:1.16.3"))
-    testRuntimeOnly("org.testcontainers:mariadb")
-    testRuntimeOnly("org.testcontainers:mssqlserver")
-    testRuntimeOnly("org.testcontainers:mysql")
-    testRuntimeOnly("org.testcontainers:postgresql")
-    when (driver) {
-        "h2" -> {
-            runtimeOnly(project(":komapper-dialect-h2-jdbc"))
-        }
-        "mariadb" -> {
-            runtimeOnly(project(":komapper-dialect-mariadb-jdbc"))
-        }
-        "mysql" -> {
-            runtimeOnly(project(":komapper-dialect-mysql-jdbc"))
-        }
-        "postgresql" -> {
-            runtimeOnly(project(":komapper-dialect-postgresql-jdbc"))
-        }
-        "sqlserver" -> {
-            runtimeOnly(project(":komapper-dialect-sqlserver-jdbc"))
-        }
-        else -> throw IllegalArgumentException("Unknown driver: $driver")
-    }
+    api(project(":integration-test-core"))
+    api(project(":komapper-tx-jdbc"))
+    api("org.postgresql:postgresql:42.3.1")
+    api(platform("org.testcontainers:testcontainers-bom:1.16.3"))
+    api("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
 tasks {
     test {
-        val urlKey = "$driver.url"
-        val url = project.property(urlKey) ?: throw GradleException("The $urlKey property is not found.")
-        this.systemProperty("driver", driver)
-        this.systemProperty("url", url)
+        enabled = false
+    }
+    check {
+        dependsOn(testing.suites.named("h2"))
+    }
+}
+
+testing {
+    suites {
+        register("h2", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly(project(":komapper-dialect-h2-jdbc"))
+            }
+        }
+
+        register("mariadb", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly("org.testcontainers:mariadb")
+                runtimeOnly(project(":komapper-dialect-mariadb-jdbc"))
+            }
+        }
+
+        register("mysql", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly("org.testcontainers:mysql")
+                runtimeOnly(project(":komapper-dialect-mysql-jdbc"))
+            }
+        }
+
+        register("postgresql", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly("org.testcontainers:postgresql")
+                runtimeOnly(project(":komapper-dialect-postgresql-jdbc"))
+            }
+        }
+
+        register("sqlserver", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly("org.testcontainers:mssqlserver")
+                runtimeOnly(project(":komapper-dialect-sqlserver-jdbc"))
+            }
+        }
+    }
+}
+
+fun JvmTestSuite.setup(driver: String) {
+    useJUnitJupiter()
+    sources {
+        java {
+            setSrcDirs(listOf("src/test/kotlin"))
+        }
+    }
+    targets {
+        all {
+            testTask.configure {
+                val urlKey = "$driver.url"
+                val url = project.property(urlKey) ?: throw GradleException("The $urlKey property is not found.")
+                systemProperty("driver", driver)
+                systemProperty("url", url)
+            }
+        }
     }
 }
--- integration-test-r2dbc/build.gradle.kts
+++ integration-test-r2dbc/build.gradle.kts
@@ -1,42 +1,92 @@
-val driver: Any by project
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    `jvm-test-suite`
+}
 
 dependencies {
-    implementation(project(":integration-test-core"))
-    implementation(project(":komapper-tx-r2dbc"))
-    testImplementation(platform("org.testcontainers:testcontainers-bom:1.16.3"))
-    testImplementation("org.testcontainers:r2dbc")
-    testImplementation("org.testcontainers:mariadb")
-    testImplementation("org.testcontainers:mssqlserver")
-    testImplementation("org.testcontainers:mysql")
-    testImplementation("org.testcontainers:postgresql")
-    when (driver) {
-        "h2" -> {
-            runtimeOnly(project(":komapper-dialect-h2-r2dbc"))
-        }
-        "mariadb" -> {
-            runtimeOnly(project(":komapper-dialect-mariadb-r2dbc"))
-            testRuntimeOnly("org.mariadb.jdbc:mariadb-java-client:2.7.5")
-        }
-        "mysql" -> {
-            runtimeOnly(project(":komapper-dialect-mysql-r2dbc"))
-            testRuntimeOnly("mysql:mysql-connector-java:8.0.28")
-        }
-        "postgresql" -> {
-            runtimeOnly(project(":komapper-dialect-postgresql-r2dbc"))
-        }
-        "sqlserver" -> {
-            runtimeOnly(project(":komapper-dialect-sqlserver-r2dbc"))
-            testRuntimeOnly("com.microsoft.sqlserver:mssql-jdbc:9.4.1.jre11")
-        }
-        else -> throw IllegalArgumentException("Unknown driver: $driver")
-    }
+    api(project(":integration-test-core"))
+    api(project(":komapper-tx-r2dbc"))
+    api(platform("org.testcontainers:testcontainers-bom:1.16.3"))
+    api("org.jetbrains.kotlin:kotlin-test-junit5")
+    api("org.testcontainers:r2dbc")
+    api("org.testcontainers:mariadb")
+    api("org.testcontainers:mssqlserver")
+    api("org.testcontainers:mysql")
+    api("org.testcontainers:postgresql")
 }
 
 tasks {
     test {
-        val urlKey = "$driver.url"
-        val url = project.property(urlKey) ?: throw GradleException("The $urlKey property is not found.")
-        this.systemProperty("driver", driver)
-        this.systemProperty("url", url)
+        enabled = false
+    }
+    check {
+        dependsOn(testing.suites.named("h2"))
+    }
+}
+
+testing {
+    suites {
+        register("h2", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly(project(":komapper-dialect-h2-r2dbc"))
+            }
+        }
+
+        register("mariadb", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly(project(":komapper-dialect-mariadb-r2dbc"))
+                runtimeOnly("org.mariadb.jdbc:mariadb-java-client:2.7.5")
+            }
+        }
+
+        register("mysql", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly(project(":komapper-dialect-mysql-r2dbc"))
+                runtimeOnly("mysql:mysql-connector-java:8.0.28")
+            }
+        }
+
+        register("postgresql", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly(project(":komapper-dialect-postgresql-r2dbc"))
+            }
+        }
+
+        register("sqlserver", JvmTestSuite::class) {
+            setup(name)
+            dependencies {
+                implementation(project)
+                runtimeOnly(project(":komapper-dialect-sqlserver-r2dbc"))
+                runtimeOnly("com.microsoft.sqlserver:mssql-jdbc:9.4.1.jre11")
+            }
+        }
+    }
+}
+
+fun JvmTestSuite.setup(driver: String) {
+    useJUnitJupiter()
+    sources {
+        java {
+            setSrcDirs(listOf("src/test/kotlin"))
+        }
+    }
+    targets {
+        all {
+            testTask.configure {
+                val urlKey = "$driver.url"
+                val url = project.property(urlKey) ?: throw GradleException("The $urlKey property is not found.")
+                systemProperty("driver", driver)
+                systemProperty("url", url)
+            }
+        }
     }
 }
