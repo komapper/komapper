2beb82b7b7f4c247157a637eb46648bbdc53615b
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-02-05 19:11:53 +0900

    Fix data mappings

--- komapper-dialect-postgresql-r2dbc/src/main/kotlin/org/komapper/dialect/postgresql/r2dbc/R2dbcPostgreSqlJsonType.kt
+++ komapper-dialect-postgresql-r2dbc/src/main/kotlin/org/komapper/dialect/postgresql/r2dbc/R2dbcPostgreSqlJsonType.kt
@@ -1,17 +1,17 @@
 package org.komapper.dialect.postgresql.r2dbc
 
-import io.r2dbc.postgresql.codec.Interval
+import io.r2dbc.postgresql.codec.Json
 import io.r2dbc.spi.Row
 import org.komapper.r2dbc.R2dbcAbstractType
 
-object R2dbcPostgreSqlJsonType : R2dbcAbstractType<Interval>(Interval::class) {
+object R2dbcPostgreSqlJsonType : R2dbcAbstractType<Json>(Json::class) {
     override val name: String = "json"
 
-    override fun getValue(row: Row, index: Int): Interval? {
+    override fun getValue(row: Row, index: Int): Json? {
         return row.get(index, klass.java)
     }
 
-    override fun getValue(row: Row, columnLabel: String): Interval? {
+    override fun getValue(row: Row, columnLabel: String): Json? {
         return row.get(columnLabel, klass.java)
     }
 }
