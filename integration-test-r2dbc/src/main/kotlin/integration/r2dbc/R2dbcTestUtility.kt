5a5a8863445f64b2385399b716f55c0569fe28e4
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-03-21 12:16:16 +0900

    Adjust transaction name for SQL Server

--- integration-test-r2dbc/src/main/kotlin/integration/r2dbc/R2dbcTestUtility.kt
+++ integration-test-r2dbc/src/main/kotlin/integration/r2dbc/R2dbcTestUtility.kt
@@ -9,8 +9,9 @@ import org.komapper.tx.core.TransactionProperty
 
 fun <T> inTransaction(db: R2dbcDatabase, info: TestInfo, block: suspend (CoroutineTransactionOperator) -> T) {
     runBlockingWithTimeout {
-        val name = TransactionProperty.Name(info.displayName)
-        db.withTransaction(transactionProperty = name) {
+        // SQL Server does not allow transaction names to be keywords or longer than 33 characters
+        val name = info.testMethod.map { it.name }.orElse("unknown").let { "komapper_$it" }.take(32)
+        db.withTransaction(transactionProperty = TransactionProperty.Name(name)) {
             it.setRollbackOnly()
             block(it)
         }
