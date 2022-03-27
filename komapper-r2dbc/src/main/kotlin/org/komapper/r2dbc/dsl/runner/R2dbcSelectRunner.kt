898e14f6b979d825625339ec4fa7f2a8d91c95cd
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-03-27 09:13:29 +0900

    Add a workaround to avoid timeouts in SQL Server (#528)

--- komapper-r2dbc/src/main/kotlin/org/komapper/r2dbc/dsl/runner/R2dbcSelectRunner.kt
+++ komapper-r2dbc/src/main/kotlin/org/komapper/r2dbc/dsl/runner/R2dbcSelectRunner.kt
@@ -2,6 +2,8 @@ package org.komapper.r2dbc.dsl.runner
 
 import io.r2dbc.spi.Row
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.toList
 import org.komapper.core.DatabaseConfig
 import org.komapper.core.DryRunStatement
 import org.komapper.core.dsl.context.SelectContext
@@ -23,7 +25,14 @@ internal class R2dbcSelectRunner<T, R>(
 
     override suspend fun run(config: R2dbcDatabaseConfig): R {
         val flow = flowBuilder.build(config)
-        return collect(flow)
+        // TODO: This is a workaround to avoid timeouts in SQL Server. However, it is unclear if this workaround is effective.
+        return if (config.dialect.driver == "sqlserver") {
+            // consume all data
+            val newFlow = flow.toList().asFlow()
+            collect(newFlow)
+        } else {
+            collect(flow)
+        }
     }
 
     override fun dryRun(config: DatabaseConfig): DryRunStatement {
