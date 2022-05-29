1a40c30aef5fc924fc27b408f303f41d7ebb482c
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-05-29 10:03:28 +0900

    Consider that an entity declaration may be from a class file (#623)
    * Consider that an entity declaration may be from a class file
    
    * Polish

--- komapper-processor/src/main/kotlin/org/komapper/processor/EntityModel.kt
+++ komapper-processor/src/main/kotlin/org/komapper/processor/EntityModel.kt
@@ -1,6 +1,39 @@
 package org.komapper.processor
 
+import com.google.devtools.ksp.symbol.KSFile
+
 internal class EntityModel(
-    val definitionSource: EntityDefinitionSource,
+    private val definitionSource: EntityDefinitionSource,
     val entity: Entity? = null,
-)
+) {
+    val hasStubAnnotation: Boolean = definitionSource.stubAnnotation != null
+
+    val entityDeclaration = definitionSource.entityDeclaration
+
+    val aliases = definitionSource.aliases.ifEmpty {
+        val alias = toCamelCase(definitionSource.entityDeclaration.simpleName.asString())
+        listOf(alias)
+    }
+
+    val typeName = definitionSource.entityDeclaration.qualifiedName?.asString() ?: ""
+
+    val containingFiles: List<KSFile>
+        get() {
+            val defDeclaration = definitionSource.defDeclaration
+            val entityDeclaration = definitionSource.entityDeclaration
+            return if (defDeclaration == entityDeclaration) {
+                listOfNotNull(defDeclaration.containingFile)
+            } else {
+                listOfNotNull(defDeclaration.containingFile, entityDeclaration.containingFile)
+            }
+        }
+
+    fun createMetamodelClassName(prefix: String, suffix: String): Pair<String, String> {
+        val defDeclaration = definitionSource.defDeclaration
+        val packageName = defDeclaration.packageName.asString()
+        val qualifiedName = defDeclaration.qualifiedName?.asString() ?: ""
+        val packageRemovedQualifiedName = qualifiedName.removePrefix("$packageName.")
+        val simpleName = prefix + packageRemovedQualifiedName.replace(".", "_") + suffix
+        return packageName to simpleName
+    }
+}
