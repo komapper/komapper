7162135c352ac43898b91a02bdfc05151a0e468d
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2022-08-29 22:02:43 +0900

    Fix according to the new ch.qos.logback.classic.spi.Configurator specifications

--- integration-test-core/src/main/kotlin/integration/core/LogConfigurator.kt
+++ integration-test-core/src/main/kotlin/integration/core/LogConfigurator.kt
@@ -4,16 +4,21 @@ import ch.qos.logback.classic.BasicConfigurator
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.spi.Configurator
 import org.komapper.core.LogCategory
 
 class LogConfigurator : BasicConfigurator() {
-    override fun configure(lc: LoggerContext) {
-        super.configure(lc)
+    override fun configure(lc: LoggerContext): Configurator.ExecutionStatus {
+        val status = super.configure(lc)
         val sqlLogger: Logger = lc.getLogger(LogCategory.SQL_WITH_ARGS)
         sqlLogger.level = Level.TRACE
         val txLogger: Logger = lc.getLogger(LogCategory.TRANSACTION)
         txLogger.level = Level.TRACE
         val rootLogger: Logger = lc.getLogger(Logger.ROOT_LOGGER_NAME)
         rootLogger.level = Level.INFO
+        return when (status) {
+            Configurator.ExecutionStatus.NEUTRAL -> Configurator.ExecutionStatus.DO_NOT_INVOKE_NEXT_IF_ANY
+            else -> status
+        }
     }
 }
