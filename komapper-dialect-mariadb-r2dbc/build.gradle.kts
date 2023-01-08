55dfd7ecc3e5e593feb9405630bdae514b14f1d8
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-01-08 12:58:35 +0900

    Remove r2dbcBomVersion

--- komapper-dialect-mariadb-r2dbc/build.gradle.kts
+++ komapper-dialect-mariadb-r2dbc/build.gradle.kts
@@ -1,7 +1,5 @@
 dependencies {
-    val r2dbcBomVersion: String by project
     api(project(":komapper-dialect-mariadb"))
     api(project(":komapper-r2dbc"))
-    implementation(platform("io.r2dbc:r2dbc-bom:$r2dbcBomVersion"))
-    implementation("org.mariadb:r2dbc-mariadb")
+    implementation("org.mariadb:r2dbc-mariadb:1.1.2")
 }
--- komapper-dialect-mysql-r2dbc/build.gradle.kts
+++ komapper-dialect-mysql-r2dbc/build.gradle.kts
@@ -1,7 +1,5 @@
 dependencies {
-    val r2dbcBomVersion: String by project
     api(project(":komapper-dialect-mysql"))
     api(project(":komapper-r2dbc"))
-    implementation(platform("io.r2dbc:r2dbc-bom:$r2dbcBomVersion"))
     implementation("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
 }
