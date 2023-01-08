d31b689362391660112161d84150de074104df28
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-01-08 13:30:25 +0900

    Transitively Expose JDBC/R2DBC driver APIs

--- komapper-dialect-sqlserver-r2dbc/build.gradle.kts
+++ komapper-dialect-sqlserver-r2dbc/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     api(project(":komapper-dialect-sqlserver"))
     api(project(":komapper-r2dbc"))
-    implementation("io.r2dbc:r2dbc-mssql:1.0.0.RELEASE")
+    api("io.r2dbc:r2dbc-mssql:1.0.0.RELEASE")
 }
