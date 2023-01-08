a8f45eff46bd4851db6599f8a37e2d5baba486e0
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-01-08 12:44:53 +0900

    Update oracle-r2dbc to v1.0.0

--- komapper-dialect-oracle-r2dbc/build.gradle.kts
+++ komapper-dialect-oracle-r2dbc/build.gradle.kts
@@ -1,7 +1,5 @@
 dependencies {
-    val r2dbcBomVersion: String by project
     api(project(":komapper-dialect-oracle"))
     api(project(":komapper-r2dbc"))
-    implementation(platform("io.r2dbc:r2dbc-bom:$r2dbcBomVersion"))
-    implementation("com.oracle.database.r2dbc:oracle-r2dbc")
+    implementation("com.oracle.database.r2dbc:oracle-r2dbc:1.0.0")
 }
