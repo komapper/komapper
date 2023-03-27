ca2ff6f551d3069ef2f3c95e905c36cce600488a
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-03-27 18:06:43 +0900

    Optimize caching of entities in EntityStore

--- komapper-core/src/main/kotlin/org/komapper/core/dsl/query/EntityStore.kt
+++ komapper-core/src/main/kotlin/org/komapper/core/dsl/query/EntityStore.kt
@@ -92,7 +92,7 @@ internal class EntityStoreImpl(
 ) : EntityStore {
 
     private val oneToManyCache: ConcurrentMap<Pair<*, *>, Map<*, Set<*>>> = ConcurrentHashMap()
-    private val manyToOneCache: ConcurrentMap<Pair<*, *>, Map<*, *>> = ConcurrentHashMap()
+    private val manyToOneCache: ConcurrentMap<Pair<*, *>, Map<EntityRef<*, *>, *>> = ConcurrentHashMap()
 
     override operator fun contains(metamodel: EntityMetamodel<*, *, *>): Boolean {
         return entitySets.containsKey(metamodel)
@@ -111,21 +111,20 @@ internal class EntityStoreImpl(
         first: EntityMetamodel<T, *, *>,
         second: EntityMetamodel<S, *, *>,
     ): Map<T, S?> {
-        return createOneToOne(first, second)
+        return createOneToOne(first, second).mapKeys { it.key.entity }
     }
 
     override fun <T : Any, ID : Any, S : Any> oneToOneById(
         first: EntityMetamodel<T, ID, *>,
         second: EntityMetamodel<S, *, *>,
     ): Map<ID, S?> {
-        val oneToOne = createOneToOne(first, second)
-        return oneToOne.mapKeys { first.extractId(it.key) }
+        return createOneToOne(first, second).mapKeys { it.key.id }
     }
 
-    private fun <T : Any, S : Any> createOneToOne(
-        first: EntityMetamodel<T, *, *>,
+    private fun <T : Any, ID : Any, S : Any> createOneToOne(
+        first: EntityMetamodel<T, ID, *>,
         second: EntityMetamodel<S, *, *>,
-    ): Map<T, S?> {
+    ): Map<EntityRef<T, ID>, S?> {
         return createManyToOne(first, second)
     }
 
@@ -133,31 +132,32 @@ internal class EntityStoreImpl(
         first: EntityMetamodel<T, *, *>,
         second: EntityMetamodel<S, *, *>,
     ): Map<T, Set<S>> {
-        return createOneToMany(first, second)
+        return createOneToMany(first, second).mapKeys { it.key.entity }
     }
 
     override fun <T : Any, ID : Any, S : Any> oneToManyById(
         first: EntityMetamodel<T, ID, *>,
         second: EntityMetamodel<S, *, *>,
     ): Map<ID, Set<S>> {
-        val oneToMany = createOneToMany(first, second)
-        return oneToMany.mapKeys { first.extractId(it.key) }
+        return createOneToMany(first, second).mapKeys { it.key.id }
     }
 
-    private fun <T : Any, S : Any> createOneToMany(
-        first: EntityMetamodel<T, *, *>,
+    private fun <T : Any, ID : Any, S : Any> createOneToMany(
+        first: EntityMetamodel<T, ID, *>,
         second: EntityMetamodel<S, *, *>,
-    ): Map<T, Set<S>> {
+    ): Map<EntityRef<T, ID>, Set<S>> {
         if (!contains(first, second)) {
             return emptyMap()
         }
         val cached = oneToManyCache.computeIfAbsent(first to second) {
-            val oneToMany = mutableMapOf<T, MutableSet<S>>()
+            val oneToMany = mutableMapOf<EntityRef<*, *>, MutableSet<S>>()
             for (row in rows) {
                 val entity1 = row[first]
                 val entity2 = row[second]
                 if (entity1 != null) {
-                    val key = first.klass().cast(entity1)
+                    val key = first.klass().cast(entity1).let {
+                        EntityRef(it, first.extractId(it))
+                    }
                     val values = oneToMany.computeIfAbsent(key) { mutableSetOf() }
                     if (entity2 != null) {
                         val value = second.klass().cast(entity2)
@@ -168,38 +168,39 @@ internal class EntityStoreImpl(
             oneToMany
         }
         @Suppress("UNCHECKED_CAST")
-        return cached as Map<T, Set<S>>
+        return cached as Map<EntityRef<T, ID>, Set<S>>
     }
 
     override fun <T : Any, S : Any> manyToOne(
         first: EntityMetamodel<T, *, *>,
         second: EntityMetamodel<S, *, *>,
     ): Map<T, S?> {
-        return createManyToOne(first, second)
+        return createManyToOne(first, second).mapKeys { it.key.entity }
     }
 
     override fun <T : Any, ID : Any, S : Any> manyToOneById(
         first: EntityMetamodel<T, ID, *>,
         second: EntityMetamodel<S, *, *>,
     ): Map<ID, S?> {
-        val manyToOne = createManyToOne(first, second)
-        return manyToOne.mapKeys { first.extractId(it.key) }
+        return createManyToOne(first, second).mapKeys { it.key.id }
     }
 
-    private fun <T : Any, S : Any> createManyToOne(
-        first: EntityMetamodel<T, *, *>,
+    private fun <T : Any, ID : Any, S : Any> createManyToOne(
+        first: EntityMetamodel<T, ID, *>,
         second: EntityMetamodel<S, *, *>,
-    ): Map<T, S?> {
+    ): Map<EntityRef<T, ID>, S?> {
         if (!contains(first, second)) {
             return emptyMap()
         }
         val cached = manyToOneCache.computeIfAbsent(first to second) {
-            val manyToOne = mutableMapOf<T, S?>()
+            val manyToOne = mutableMapOf<EntityRef<*, *>, S?>()
             for (row in rows) {
                 val entity1 = row[first]
                 val entity2 = row[second]
                 if (entity1 != null) {
-                    val key = first.klass().cast(entity1)
+                    val key = first.klass().cast(entity1).let {
+                        EntityRef(it, first.extractId(it))
+                    }
                     val value = entity2?.let { second.klass().cast(it) }
                     manyToOne[key] = value
                 }
@@ -207,6 +208,23 @@ internal class EntityStoreImpl(
             manyToOne
         }
         @Suppress("UNCHECKED_CAST")
-        return cached as Map<T, S?>
+        return cached as Map<EntityRef<T, ID>, S?>
+    }
+}
+
+private class EntityRef<ENTITY : Any, ID : Any>(val entity: ENTITY, val id: ID) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as EntityRef<*, *>
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
     }
 }
