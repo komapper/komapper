29deb066075f786616e1acb8681b1c1daaad39b9
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-04-08 17:06:11 +0900

    Remove unnecessary settings from build.gradle.kts

--- example-basic-r2dbc/build.gradle.kts
+++ example-basic-r2dbc/build.gradle.kts
@@ -15,14 +15,6 @@ application {
     mainClass.set("example.basic.r2dbc.ApplicationKt")
 }
 
-idea {
-    module {
-        sourceDirs = sourceDirs + file("build/generated/ksp/main/kotlin")
-        testSourceDirs = testSourceDirs + file("build/generated/ksp/test/kotlin")
-        generatedSourceDirs = generatedSourceDirs + file("build/generated/ksp/main/kotlin") + file("build/generated/ksp/test/kotlin")
-    }
-}
-
 ksp {
     arg("komapper.namingStrategy", "UPPER_SNAKE_CASE")
 }
