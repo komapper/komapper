44e1dadf89132ba84be4df73d8074f2d20529f4f
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-04-27 21:21:06 +0900

    Polish statement builders for H2 (#974)

--- komapper-dialect-h2/src/main/kotlin/org/komapper/dialect/h2/H2EntityInsertStatementBuilder.kt
+++ komapper-dialect-h2/src/main/kotlin/org/komapper/dialect/h2/H2EntityInsertStatementBuilder.kt
@@ -6,38 +6,22 @@ import org.komapper.core.StatementBuffer
 import org.komapper.core.dsl.builder.DefaultEntityInsertStatementBuilder
 import org.komapper.core.dsl.builder.EntityInsertStatementBuilder
 import org.komapper.core.dsl.context.EntityInsertContext
-import org.komapper.core.dsl.expression.ColumnExpression
 import org.komapper.core.dsl.metamodel.EntityMetamodel
 
 class H2EntityInsertStatementBuilder<ENTITY : Any, ID : Any, META : EntityMetamodel<ENTITY, ID, META>>(
-    private val dialect: BuilderDialect,
-    private val context: EntityInsertContext<ENTITY, ID, META>,
+    dialect: BuilderDialect,
+    context: EntityInsertContext<ENTITY, ID, META>,
     entities: List<ENTITY>,
 ) : EntityInsertStatementBuilder<ENTITY, ID, META> {
 
     private val buf = StatementBuffer()
     private val builder = DefaultEntityInsertStatementBuilder(dialect, context, entities)
+    private val support = H2StatementBuilderSupport(dialect, context)
 
     override fun build(): Statement {
-        val outputExpressions = context.returning.expressions()
-        if (outputExpressions.isNotEmpty()) {
-            buf.append("select ")
-            for (e in outputExpressions) {
-                column(e)
-                buf.append(", ")
-            }
-            buf.cutBack(2)
-            buf.append(" from final table (")
-        }
+        buf.append(support.buildReturningFirstFragment())
         buf.append(builder.build())
-        if (outputExpressions.isNotEmpty()) {
-            buf.append(")")
-        }
+        buf.append(support.buildReturningLastFragment())
         return buf.toStatement()
-    }
-
-    private fun column(expression: ColumnExpression<*, *>) {
-        val name = expression.getCanonicalColumnName(dialect::enquote)
-        buf.append(name)
     }
 }
--- komapper-dialect-h2/src/main/kotlin/org/komapper/dialect/h2/H2RelationInsertValuesStatementBuilder.kt
+++ komapper-dialect-h2/src/main/kotlin/org/komapper/dialect/h2/H2RelationInsertValuesStatementBuilder.kt
@@ -5,38 +5,22 @@ import org.komapper.core.Statement
 import org.komapper.core.StatementBuffer
 import org.komapper.core.dsl.builder.RelationInsertValuesStatementBuilder
 import org.komapper.core.dsl.context.RelationInsertValuesContext
-import org.komapper.core.dsl.expression.ColumnExpression
 import org.komapper.core.dsl.expression.Operand
 import org.komapper.core.dsl.metamodel.EntityMetamodel
 import org.komapper.core.dsl.metamodel.PropertyMetamodel
 
 class H2RelationInsertValuesStatementBuilder<ENTITY : Any, ID : Any, META : EntityMetamodel<ENTITY, ID, META>>(
-    private val dialect: BuilderDialect,
-    private val context: RelationInsertValuesContext<ENTITY, ID, META>,
+    dialect: BuilderDialect,
+    context: RelationInsertValuesContext<ENTITY, ID, META>,
 ) : RelationInsertValuesStatementBuilder<ENTITY, ID, META> {
     private val buf = StatementBuffer()
     private val builder = RelationInsertValuesStatementBuilder(dialect, context)
+    private val support = H2StatementBuilderSupport(dialect, context)
 
     override fun build(assignments: List<Pair<PropertyMetamodel<ENTITY, *, *>, Operand>>): Statement {
-        val outputExpressions = context.returning.expressions()
-        if (outputExpressions.isNotEmpty()) {
-            buf.append("select ")
-            for (e in outputExpressions) {
-                column(e)
-                buf.append(", ")
-            }
-            buf.cutBack(2)
-            buf.append(" from final table (")
-        }
+        buf.append(support.buildReturningFirstFragment())
         buf.append(builder.build(assignments))
-        if (outputExpressions.isNotEmpty()) {
-            buf.append(")")
-        }
+        buf.append(support.buildReturningLastFragment())
         return buf.toStatement()
-    }
-
-    private fun column(expression: ColumnExpression<*, *>) {
-        val name = expression.getCanonicalColumnName(dialect::enquote)
-        buf.append(name)
     }
 }
