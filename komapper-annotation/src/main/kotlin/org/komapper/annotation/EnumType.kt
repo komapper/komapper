a083ae771602866f4206fe6390839e6581d23923
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-09-23 21:00:35 +0900

    Format

--- komapper-annotation/src/main/kotlin/org/komapper/annotation/EnumType.kt
+++ komapper-annotation/src/main/kotlin/org/komapper/annotation/EnumType.kt
@@ -21,8 +21,8 @@ enum class EnumType {
 
     /**
      * The enum type itself is mapped to database.
-     * 
-     * You need to register a user-defined data type corresponding to the Enum type. 
+     *
+     * You need to register a user-defined data type corresponding to the Enum type.
      * See [User-defined data types](https://www.komapper.org/docs/reference/data-type/#user-defined-data-types).
      * See [JDBC example](https://github.com/komapper/komapper/blob/main/integration-test-jdbc/src/main/kotlin/integration/jdbc/MoodType.kt).
      * See [R2DBC example](https://github.com/komapper/komapper/blob/main/integration-test-r2dbc/src/main/kotlin/integration/r2dbc/MoodType.kt).
