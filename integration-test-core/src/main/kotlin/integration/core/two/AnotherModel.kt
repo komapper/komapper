57872194839cd1c03fa3e8b533b17b0398e6a78f
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2023-10-07 06:46:06 +0900

    Fix an error that occurred while associating an object from another package

---
+++ integration-test-core/src/main/kotlin/integration/core/two/AnotherModel.kt
@@ -0,0 +1,14 @@
+package integration.core.two
+
+import integration.core.one.MyTestModel
+import org.komapper.annotation.KomapperEntity
+import org.komapper.annotation.KomapperId
+import org.komapper.annotation.KomapperOneToMany
+
+@KomapperOneToMany(MyTestModel::class, "mymodel")
+@KomapperEntity
+data class AnotherModel(
+    @KomapperId
+    val id: Long,
+    val myModelId: Long,
+)
