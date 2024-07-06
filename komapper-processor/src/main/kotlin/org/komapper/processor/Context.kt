46d4df48606c6630bedc64eb58a3f59b444d8baf
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2024-07-06 20:19:04 +0900

    Add location information to the output of KSPLogger (#1307)
    * Add location information to the output of KSPLogger
    
    * Remove a redundant slash

--- komapper-processor/src/main/kotlin/org/komapper/processor/Context.kt
+++ komapper-processor/src/main/kotlin/org/komapper/processor/Context.kt
@@ -4,10 +4,10 @@ import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 
 internal data class Context(
-    val environment: SymbolProcessorEnvironment,
+    private val environment: SymbolProcessorEnvironment,
     val config: Config,
     val resolver: Resolver,
 ) {
-    val logger get() = environment.logger
+    val logger = KomapperKSPLogger(environment.logger)
     val codeGenerator get() = environment.codeGenerator
 }
---
+++ komapper-processor/src/main/kotlin/org/komapper/processor/KomapperKSPLogger.kt
@@ -0,0 +1,36 @@
+package org.komapper.processor
+
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.symbol.FileLocation
+import com.google.devtools.ksp.symbol.KSNode
+import com.google.devtools.ksp.symbol.NonExistLocation
+
+class KomapperKSPLogger(private val logger: KSPLogger) : KSPLogger {
+
+    override fun logging(message: String, symbol: KSNode?) {
+        logger.logging(convertMessage(message, symbol))
+    }
+
+    override fun info(message: String, symbol: KSNode?) {
+        logger.info(convertMessage(message, symbol))
+    }
+
+    override fun warn(message: String, symbol: KSNode?) {
+        logger.warn(convertMessage(message, symbol))
+    }
+
+    override fun error(message: String, symbol: KSNode?) {
+        logger.error(convertMessage(message, symbol))
+    }
+
+    override fun exception(e: Throwable) {
+        logger.exception(e)
+    }
+
+    private fun convertMessage(message: String, symbol: KSNode?): String {
+        return when (val location = symbol?.location) {
+            is FileLocation -> "$message file://${location.filePath}:${location.lineNumber}"
+            is NonExistLocation, null -> message
+        }
+    }
+}
