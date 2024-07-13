a749a8cd196b3f06f3fcf155fdf447286f6b7053
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2024-07-13 12:16:01 +0900

    Use the compilerOptions property (#1315)

--- integration-test-jdbc/build.gradle.kts
+++ integration-test-jdbc/build.gradle.kts
@@ -39,10 +39,10 @@ tasks {
         )
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions.freeCompilerArgs += listOf(
-            "-opt-in=org.komapper.annotation.KomapperExperimentalAssociation",
-            "-Xcontext-receivers",
-        )
+        compilerOptions {
+            freeCompilerArgs.add("-opt-in=org.komapper.annotation.KomapperExperimentalAssociation")
+            freeCompilerArgs.add("-Xcontext-receivers")
+        }
     }
 }
 
