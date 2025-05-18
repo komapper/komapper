b6ec40946e5f7eb223cb3bd67a5d28b0f948911f
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2025-05-19 00:13:36 +0900

    Remove SQLCommenter integration from the project.
    Because sqlcommenter does not support Spring 6 / Spring Boot 3.
    See https://github.com/google/sqlcommenter/issues/234.

--- example-spring-boot-jdbc/src/main/kotlin/example/spring/boot/jdbc/WebConfig.kt
+++ example-spring-boot-jdbc/src/main/kotlin/example/spring/boot/jdbc/WebConfig.kt
@@ -1,21 +1,9 @@
 package example.spring.boot.jdbc
 
-import com.google.cloud.sqlcommenter.interceptors.SpringSQLCommenterInterceptor
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @EnableWebMvc
 @Configuration
-class WebConfig : WebMvcConfigurer {
-    @Bean
-    fun sqlInterceptor(): SpringSQLCommenterInterceptor {
-        return SpringSQLCommenterInterceptor()
-    }
-
-    override fun addInterceptors(registry: InterceptorRegistry) {
-        registry.addInterceptor(sqlInterceptor())
-    }
-}
+class WebConfig : WebMvcConfigurer
